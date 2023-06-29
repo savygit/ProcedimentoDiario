bbd97e1c628f8a02b16e017813f427529572f711
David Soares Celestino Junior <ds.celestino.jr@gmail.com>
2023-06-29 10:02:47 -0300

    Remoção de arquivos que são recriados em todas as preparações de atualização

--- ultimosCommitsMaster.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\xampp\htdocs\Trix\Oficial2.0\"
-git log --pretty=format:"" master --name-only --since="2022-07-15" > C:\xampp\htdocs\Trix\\OutrosProgramas\ProcedimentoDiario\\logcommit.txt
-cmd
