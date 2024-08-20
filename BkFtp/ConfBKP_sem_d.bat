fe4629be7d09e9a7391b7b5939d3bf95008746d4
Giovana <giovana@savy.com.br>
2024-08-20 08:39:47 -0300

    Atualização do arquivo

--- BkFtp/ConfBKP_sem_d.bat
+++ BkFtp/ConfBKP_sem_d.bat
@@ -17,8 +17,6 @@ mkdir BACKUP\BK_%1\SP_CTeMDFe
 mkdir BACKUP\BK_%1\Toride
 mkdir BACKUP\BK_%1\Sigbol
 mkdir BACKUP\BK_%1\FS_SCS
-mkdir BACKUP\BK_%1\FS_Moema
-mkdir BACKUP\BK_%1\FS_Santos
 mkdir BACKUP\BK_%1\FS_Controle
 mkdir BACKUP\BK_%1\SiteSigbol
 mkdir BACKUP\BK_%1\Savy
@@ -31,17 +29,18 @@ mkdir BACKUP\BK_%1\Proserv
 mkdir BACKUP\BK_%1\LAFF
 mkdir BACKUP\BK_%1\BrasconLogAPP
 mkdir BACKUP\BK_%1\AppColetasBrascon
-mkdir BACKUP\BK_%1\BrasconPombos
 mkdir BACKUP\BK_%1\BrasconJaparatuba
-mkdir BACKUP\BK_%1\BrasconItapicuru
-mkdir BACKUP\BK_%1\BrasconSaoGoncalo
-mkdir BACKUP\BK_%1\Sterlix
-mkdir BACKUP\BK_%1\Ecoservice
-mkdir BACKUP\BK_%1\NtSterlixParnaiba
-mkdir BACKUP\BK_%1\NtSterlix
-mkdir BACKUP\BK_%1\NtRaiz
+mkdir BACKUP\BK_%1\Fixx
+mkdir BACKUP\BK_%1\Ampa
+mkdir BACKUP\BK_%1\Amav
+mkdir BACKUP\BK_%1\Educam
 mkdir BACKUP\BK_%1\PHPInfo
 mkdir BACKUP\BK_%1\LogsQ_Reflectsys
+mkdir BACKUP\BK_%1\NFeCJDias
+mkdir BACKUP\BK_%1\NFeMCRibeiro
+mkdir BACKUP\BK_%1\NFeMQPrado
+mkdir BACKUP\BK_%1\NFeEliene
+mkdir BACKUP\BK_%1\NFeSilvano
 
 move phpInfo_trix.txt BACKUP\BK_%1\PHPInfo\phpInfo_trix.txt
 move phpInfo_mobtrix.txt BACKUP\BK_%1\PHPInfo\phpInfo_mobtrix.txt
@@ -64,24 +63,24 @@ move AppColetasJap.zip BACKUP\BK_%1\AppColetasBrascon\AppColetasJap.zip
 move bdbralog.sql.tar.gz BACKUP\BK_%1\BrasconLogAPP\bdbralog.sql.tar.gz
 move bdbrascon.sql.tar.gz BACKUP\BK_%1\BrasconPombos\bdbrascon.sql.tar.gz
 move bdbrajap.sql.tar.gz BACKUP\BK_%1\BrasconJaparatuba\bdbrajap.sql.tar.gz
-move bdbraita.sql.tar.gz BACKUP\BK_%1\BrasconItapicuru\bdbraita.sql.tar.gz
-move bdbrasga.sql.tar.gz BACKUP\BK_%1\BrasconSaoGoncalo\bdbrasga.sql.tar.gz
-move bdecoser.sql.tar.gz BACKUP\BK_%1\Ecoservice\bdecoser.sql.tar.gz
-move bdsterlix.sql.tar.gz BACKUP\BK_%1\Sterlix\bdsterlix.sql.tar.gz
-move bdntparnaiba.sql.tar.gz BACKUP\BK_%1\NtSterlixParnaiba\bdntparnaiba.sql.tar.gz
-move bdntsterlix.sql.tar.gz BACKUP\BK_%1\NtSterlix\bdntsterlix.sql.tar.gz
-move bdntraiz.sql.tar.gz BACKUP\BK_%1\NtRaiz\bdntraiz.sql.tar.gz
 move bdsigbol.sql.tar.gz BACKUP\BK_%1\Sigbol\bdsigbol.sql.tar.gz
-move bdsigmoe.sql.tar.gz BACKUP\BK_%1\FS_Moema\bdsigmoe.sql.tar.gz
-move bdsigsan.sql.tar.gz BACKUP\BK_%1\FS_Santos\bdsigsan.sql.tar.gz
 move bdsigscs.sql.tar.gz BACKUP\BK_%1\FS_SCS\bdsigscs.sql.tar.gz
 move bdsigcfr.sql.tar.gz BACKUP\BK_%1\FS_Controle\bdsigcfr.sql.tar.gz
 move sigbol1.sql.tar.gz BACKUP\BK_%1\SiteSigbol\sigbol1.sql.tar.gz 
 move bdtrixdes.sql.tar.gz BACKUP\BK_%1\TrixDes\bdtrixdes.sql.tar.gz
 move bdtrixdes2.sql.tar.gz BACKUP\BK_%1\TrixDes2\bdtrixdes2.sql.tar.gz
+move bdfixx.sql.tar.gz BACKUP\BK_%1\Fixx\bdfixx.sql.tar.gz
+move bdampa.sql.tar.gz BACKUP\BK_%1\Ampa\bdampa.sql.tar.gz
+move bdamav.sql.tar.gz BACKUP\BK_%1\Amav\bdamav.sql.tar.gz
+move bdeducam.sql.tar.gz BACKUP\BK_%1\Educam\bdeducam.sql.tar.gz
 move TorideBK.rar BACKUP\BK_%1\Toride\TorideBK.rar
 move BKPLogsQ.zip BACKUP\BK_%1\LogsQ_Reflectsys\BKPLogsQ.zip
 move BKP_DocsFiscais_SPModal.tar.gz BACKUP\BK_%1\SP_CTeMDFe\BKP_DocsFiscais_SPModal.tar.gz
+move bdnfe0001.sql.tar.gz BACKUP\BK_%1\NFeCJDias\bdnfe0001.sql.tar.gz
+move bdnfe0002.sql.tar.gz BACKUP\BK_%1\NFeSilvano\bdnfe0002.sql.tar.gz
+move bdnfe0003.sql.tar.gz BACKUP\BK_%1\NFeMQPrado\bdnfe0003.sql.tar.gz
+move bdnfe0004.sql.tar.gz BACKUP\BK_%1\NFeMCRibeiro\bdnfe0004.sql.tar.gz
+move bdnfe0005.sql.tar.gz BACKUP\BK_%1\NFeEliene\bdnfe0005.sql.tar.gz
 
 cd BACKUP
 
