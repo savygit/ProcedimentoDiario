7091f2f1dd11c58a0a2abdbce6c48baafcd594f6
Giovana <giovana@savy.com.br>
2025-03-21 08:11:47 -0300

    atualização

--- BkFtp/ConfBKP_sem_d.bat
+++ BkFtp/ConfBKP_sem_d.bat
@@ -31,9 +31,6 @@ mkdir BACKUP\BK_%1\BrasconLogAPP
 mkdir BACKUP\BK_%1\AppColetasBrascon
 mkdir BACKUP\BK_%1\BrasconJaparatuba
 mkdir BACKUP\BK_%1\Fixx
-mkdir BACKUP\BK_%1\Ampa
-mkdir BACKUP\BK_%1\Amav
-mkdir BACKUP\BK_%1\Educam
 mkdir BACKUP\BK_%1\PHPInfo
 mkdir BACKUP\BK_%1\LogsQ_Reflectsys
 mkdir BACKUP\BK_%1\NFeCJDias
@@ -41,9 +38,12 @@ mkdir BACKUP\BK_%1\NFeMCRibeiro
 mkdir BACKUP\BK_%1\NFeMQPrado
 mkdir BACKUP\BK_%1\NFeEliene
 mkdir BACKUP\BK_%1\NFeSilvano
+mkdir BACKUP\BK_%1\NFeIntermares
+mkdir BACKUP\BK_%1\NFeAdrianaBatista
+mkdir BACKUP\BK_%1\NFeHenrimar
+mkdir BACKUP\BK_%1\MBL
+mkdir BACKUP\BK_%1\Ruiz
 
-move phpInfo_trix.txt BACKUP\BK_%1\PHPInfo\phpInfo_trix.txt
-move phpInfo_mobtrix.txt BACKUP\BK_%1\PHPInfo\phpInfo_mobtrix.txt
 move phpInfo_reflectsys.txt BACKUP\BK_%1\PHPInfo\phpInfo_reflectsys.txt
 move phpInfo_reflectsys1.txt BACKUP\BK_%1\PHPInfo\phpInfo_reflectsys1.txt
 move phpInfo_reflectsys2.txt BACKUP\BK_%1\PHPInfo\phpInfo_reflectsys2.txt
@@ -58,10 +58,8 @@ move bdsavy.sql.tar.gz BACKUP\BK_%1\Savy\bdsavy.sql.tar.gz
 move bdaipagm.sql.tar.gz BACKUP\BK_%1\AIPA\bdaipagm.sql.tar.gz
 move bdproser.sql.tar.gz BACKUP\BK_%1\Proserv\bdproser.sql.tar.gz 
 move bdescmus.sql.tar.gz BACKUP\BK_%1\LAFF\bdescmus.sql.tar.gz
-move AppColetas.zip BACKUP\BK_%1\AppColetasBrascon\AppColetas.zip
 move AppColetasJap.zip BACKUP\BK_%1\AppColetasBrascon\AppColetasJap.zip
 move bdbralog.sql.tar.gz BACKUP\BK_%1\BrasconLogAPP\bdbralog.sql.tar.gz
-move bdbrascon.sql.tar.gz BACKUP\BK_%1\BrasconPombos\bdbrascon.sql.tar.gz
 move bdbrajap.sql.tar.gz BACKUP\BK_%1\BrasconJaparatuba\bdbrajap.sql.tar.gz
 move bdsigbol.sql.tar.gz BACKUP\BK_%1\Sigbol\bdsigbol.sql.tar.gz
 move bdsigscs.sql.tar.gz BACKUP\BK_%1\FS_SCS\bdsigscs.sql.tar.gz
@@ -70,9 +68,6 @@ move sigbol1.sql.tar.gz BACKUP\BK_%1\SiteSigbol\sigbol1.sql.tar.gz
 move bdtrixdes.sql.tar.gz BACKUP\BK_%1\TrixDes\bdtrixdes.sql.tar.gz
 move bdtrixdes2.sql.tar.gz BACKUP\BK_%1\TrixDes2\bdtrixdes2.sql.tar.gz
 move bdfixx.sql.tar.gz BACKUP\BK_%1\Fixx\bdfixx.sql.tar.gz
-move bdampa.sql.tar.gz BACKUP\BK_%1\Ampa\bdampa.sql.tar.gz
-move bdamav.sql.tar.gz BACKUP\BK_%1\Amav\bdamav.sql.tar.gz
-move bdeducam.sql.tar.gz BACKUP\BK_%1\Educam\bdeducam.sql.tar.gz
 move TorideBK.rar BACKUP\BK_%1\Toride\TorideBK.rar
 move BKPLogsQ.zip BACKUP\BK_%1\LogsQ_Reflectsys\BKPLogsQ.zip
 move BKP_DocsFiscais_SPModal.tar.gz BACKUP\BK_%1\SP_CTeMDFe\BKP_DocsFiscais_SPModal.tar.gz
@@ -81,6 +76,11 @@ move bdnfe0002.sql.tar.gz BACKUP\BK_%1\NFeSilvano\bdnfe0002.sql.tar.gz
 move bdnfe0003.sql.tar.gz BACKUP\BK_%1\NFeMQPrado\bdnfe0003.sql.tar.gz
 move bdnfe0004.sql.tar.gz BACKUP\BK_%1\NFeMCRibeiro\bdnfe0004.sql.tar.gz
 move bdnfe0005.sql.tar.gz BACKUP\BK_%1\NFeEliene\bdnfe0005.sql.tar.gz
+move bdnfe0006.sql.tar.gz BACKUP\BK_%1\NFeIntermares\bdnfe0006.sql.tar.gz
+move bdnfe0007.sql.tar.gz BACKUP\BK_%1\NFeAdrianaBatista\bdnfe0007.sql.tar.gz
+move bdnfe0008.sql.tar.gz BACKUP\BK_%1\NFeHenrimar\bdnfe0008.sql.tar.gz
+move bdmbltrix.sql.tar.gz BACKUP\BK_%1\MBL\bdmbltrix.sql.tar.gz
+move bdruiz.sql.tar.gz BACKUP\BK_%1\Ruiz\bdruiz.sql.tar.gz
 
 cd BACKUP
 
@@ -93,3 +93,10 @@ copy BK_%1.rar %3
 rd BK_%1/s
 
 echo ACABOU
+
+cd C:\xampp\htdocs\Trix\ProcedimentoDiario\BkFtp
+
+del DelBKP.bat
+del BaixaBkp.bat
+del BaixaBkpTor.bat
+del ConfBKP.bat
